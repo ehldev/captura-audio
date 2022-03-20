0a74a870a714f55afc34901159f9e16409b4ab49
Erick Huamani <dev.erick95@gmail.com>
2022-03-20 00:57:10 -0500

    Grabar y reproducir audio

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const recorder = document.getElementById('recorder')
+const player = document.getElementById('player')
+
+recorder.addEventListener('change', e => {
+    const file = e.target.files[0]
+    const url = URL.createObjectURL(file)
+
+    player.url = url
+})
