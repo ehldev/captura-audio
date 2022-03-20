c23fb850562e8c666b99c93e22e6a991e2a36169
Erick Huamani <dev.erick95@gmail.com>
2022-03-20 01:11:11 -0500

    Play to record

--- js/app.js
+++ js/app.js
@@ -5,8 +5,5 @@ recorder.addEventListener('change', e => {
     const file = e.target.files[0]
     const url = URL.createObjectURL(file)
 
-    document.write(url)
-
-    player.url = url
-    player.play()
+    player.src = url
 })
